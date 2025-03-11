e217718194cf25c657bdd66152efb6ea39d7343e
Joe Betz <jpbetz@google.com>
2025-03-11 11:21:40 -0400

    RC.Spec.Relicas to a pointer: update generated code

--- staging/src/k8s.io/client-go/applyconfigurations/internal/internal.go
+++ staging/src/k8s.io/client-go/applyconfigurations/internal/internal.go
@@ -7579,9 +7579,11 @@ var schemaYAML = typed.YAMLObject(`types:
     - name: minReadySeconds
       type:
         scalar: numeric
+      default: 0
     - name: replicas
       type:
         scalar: numeric
+      default: 1
     - name: selector
       type:
         map:
