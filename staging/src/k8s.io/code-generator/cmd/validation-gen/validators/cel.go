370c882070545d112a1ed94f4778803c40e0ae49
Joe Betz <jpbetz@google.com>
2025-04-04 16:54:24 -0400

    Fix up expression

--- staging/src/k8s.io/code-generator/cmd/validation-gen/validators/cel.go
+++ staging/src/k8s.io/code-generator/cmd/validation-gen/validators/cel.go
@@ -21,10 +21,8 @@ import (
 	"fmt"
 	"github.com/google/cel-go/cel"
 	"k8s.io/apimachinery/pkg/api/validate"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/gengo/v2/types"
-	pointer "k8s.io/utils/ptr"
 )
 
 const (
@@ -64,16 +62,23 @@ var newRule = types.Name{Package: libValidationPkg, Name: "NewRule"}
 // TODO: Figure out how to handle multi-line expressions.
 
 type expressionParams struct {
-	Expression        string               `json:"expression"`
-	Message           string               `json:"message,omitempty"`
-	MessageExpression string               `json:"messageExpression,omitempty"`
-	Reason            *metav1.StatusReason `json:"reason,omitempty"`
+	Expression        string `json:"expression"`
+	Message           string `json:"message,omitempty"`
+	MessageExpression string `json:"messageExpression,omitempty"`
+	Reason            string `json:"reason,omitempty"`
 }
 
 var supportedPolicyReason = sets.New(
-	metav1.StatusReasonForbidden,
-	metav1.StatusReasonInvalid,
-	metav1.StatusReasonRequestEntityTooLarge,
+	"NotFound",
+	"Required",
+	"Duplicate",
+	"Invalid",
+	"NotSupported",
+	"Forbidden",
+	"TooLong",
+	"TooMany",
+	"Internal",
+	"TypeInvalid",
 )
 
 func (stv ruleTagValidator) GetValidations(context Context, args []string, payload string) (Validations, error) {
@@ -108,12 +113,11 @@ func (stv ruleTagValidator) GetValidations(context Context, args []string, paylo
 		}
 
 	}
-	if p.Reason == nil {
-		p.Reason = pointer.To(metav1.StatusReasonInvalid)
-	} else {
-		if p.Reason != nil && !supportedPolicyReason.Has(*p.Reason) {
-			return Validations{}, fmt.Errorf("reason %q not supported", *p.Reason)
-		}
+	if p.Reason == "" {
+		p.Reason = "Invalid"
+	}
+	if !supportedPolicyReason.Has(p.Reason) {
+		return Validations{}, fmt.Errorf("reason %q not supported", p.Reason)
 	}
 
 	// TODO: Check estimated cost against a sane limit.
@@ -124,11 +128,7 @@ func (stv ruleTagValidator) GetValidations(context Context, args []string, paylo
 	//       The correct package would be the output package but is not known here. This does not show up in generated code.
 	// TODO: Append a consistent hash suffix to avoid generated name conflicts?
 	supportVarName := PrivateVar{Name: "ProgramFor" + context.Type.Name.Name, Package: "local"}
-	reason := ""
-	if p.Reason != nil {
-		reason = (string)(*p.Reason)
-	}
-	supportVar := Variable(supportVarName, Function(ruleTagName, DefaultFlags, newRule, p.Expression, p.MessageExpression, p.Message, reason))
+	supportVar := Variable(supportVarName, Function(ruleTagName, DefaultFlags, newRule, p.Expression, p.MessageExpression, p.Message, p.Reason))
 	result.AddVariable(supportVar)
 	fn := Function(ruleTagName, DefaultFlags, validateExpression, supportVarName)
 	result.AddFunction(fn)
