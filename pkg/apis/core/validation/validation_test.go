ac912d6fa893bf341494650ebf488c4de5d930a2
Joe Betz <jpbetz@google.com>
2026-06-10 09:37:31 -0400

    Move Pod conversion behaviors to defaulting

--- pkg/apis/core/validation/validation_test.go
+++ pkg/apis/core/validation/validation_test.go
@@ -23530,6 +23530,7 @@ func TestValidateOSFields(t *testing.T) {
 		"RuntimeClassName",
 		"SchedulerName",
 		"SchedulingGates[*].Name",
+		"DeprecatedServiceAccount",
 		"SecurityContext.RunAsNonRoot",
 		"ServiceAccountName",
 		"SetHostnameAsFQDN",
